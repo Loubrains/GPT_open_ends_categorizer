2add58bc19bb78c77246a7ba4d05ed4f87cf540c
Loubrains <louie.atk@gmail.com>
2023-12-25 01:39:57 +0000

    It works! Just need to get around the token limit.

--- gpt_categorizer.py
+++ gpt_categorizer.py
@@ -1,9 +1,8 @@
 from openai import OpenAI
 import pandas as pd
-import json
 import re
 import chardet
-import random
+from concurrent.futures import ThreadPoolExecutor, as_completed
 
 ### NOTE: Make sure OpenAI_API_KEY is set up in your system environment variables ###
 client = OpenAI()
@@ -17,44 +16,6 @@ def preprocess_text(text) -> str:
     text = re.sub(r"[^a-z0-9\s]", "", text)
     text = text.strip()
     return text
-
-
-def categorize_missing_data(categorized_data: pd.DataFrame) -> pd.DataFrame:
-    def is_missing(value):
-        return pd.isna(value) or value is None or value == "missing data" or value == "nan"
-
-    # Boolean mask where each row is True if all elements are missing
-    all_missing_mask = df_preprocessed.map(is_missing).all(axis=1)  # type: ignore
-    categorized_data.loc[all_missing_mask, "Missing data"] = 1
-    categorized_data.loc[all_missing_mask, "Uncategorized"] = 0
-    return categorized_data
-
-
-def categorize_response_in_dataframe(
-    response: str,
-    category: str,
-    categorized_data: pd.DataFrame,
-    response_columns: list[str],
-) -> pd.DataFrame:
-    # Boolean mask for rows in categorized_data containing selected responses
-    mask = pd.Series([False] * len(categorized_data))
-
-    for column in categorized_data[response_columns]:
-        mask |= categorized_data[column] == response
-
-    categorized_data.loc[mask, "Uncategorized"] = 0
-    categorized_data.loc[mask, category] = 1
-
-    return categorized_data
-
-
-def export_dataframe_to_csv(file_path: str, export_df: pd.DataFrame) -> None:
-    try:
-        if export_df.empty:
-            return
-        export_df.to_csv(file_path, index=False, header=False)
-    except Exception as e:
-        print(f"Error while writing to CSV: {e}")
 
 
 def categorize_response_GPT(
@@ -75,22 +36,83 @@ def categorize_response_GPT(
         {
             "role": "user",
             "content": f"""Please categorize the following questionnaire response. 
-            Use only one of the provided categories for your classification.\n\n
+            Use only one of the provided categories for your classification.
+            Don't use "Other" unless the provided categories do not suffice.\n\n
             Question:\n{question}\n\n
             Response:\n{response}\n\n
             Categories:\n{categories}\n\n
-            Return only the category that best fits the response.""",
+            Return ONLY the category name, in the format: "name" """,
         }
     ]
     try:
-        completion = client.chat.completions.create(messages=system + user, model="gpt-4")
-        category = json.loads(completion.choices[0].message.content)
+        completion = client.chat.completions.create(
+            messages=system + user, model="gpt-4-1106-preview"
+        )
+        category = completion.choices[0].message.content.strip('" ')
 
     except Exception as e:
         print(f"An error occurred: {e}")
         category = "Error"
 
     return category
+
+
+def parallel_gpt_calls(responses, client, question, categories_list):
+    categorized_responses = {}
+
+    def categorize_single_response(response):
+        return response, categorize_response_GPT(client, question, response, categories_list)
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        future_to_response = {
+            executor.submit(categorize_single_response, response): response
+            for response in responses
+        }
+        for future in as_completed(future_to_response):
+            response = future_to_response[future]
+            try:
+                category = future.result()[1]
+                categorized_responses[response] = category
+            except Exception as e:
+                print(f"Error processing response '{response}': {e}")
+
+    return categorized_responses
+
+
+def categorize_response_in_dataframe(
+    response: str,
+    category: str,
+    categorized_data: pd.DataFrame,
+    response_columns: list[str],
+) -> pd.DataFrame:
+    # Boolean mask for rows in categorized_data containing selected responses
+    mask = pd.Series([False] * len(categorized_data))
+
+    for column in categorized_data[response_columns]:
+        mask |= categorized_data[column] == response
+
+    categorized_data.loc[mask, "Uncategorized"] = 0
+    categorized_data.loc[mask, category] = 1
+
+
+def categorize_missing_data(categorized_data: pd.DataFrame) -> pd.DataFrame:
+    def is_missing(value):
+        return pd.isna(value) or value is None or value == "missing data" or value == "nan"
+
+    # Boolean mask where each row is True if all elements are missing
+    all_missing_mask = df_preprocessed.map(is_missing).all(axis=1)  # type: ignore
+    categorized_data.loc[all_missing_mask, "Missing data"] = 1
+    categorized_data.loc[all_missing_mask, "Uncategorized"] = 0
+    return categorized_data
+
+
+def export_dataframe_to_csv(file_path: str, export_df: pd.DataFrame, header: bool = False) -> None:
+    try:
+        if export_df.empty:
+            return
+        export_df.to_csv(file_path, index=False, header=header)
+    except Exception as e:
+        print(f"Error while writing to CSV: {e}")
 
 
 # Load data
@@ -101,24 +123,21 @@ with open(data_file_path, "rb") as file:
 df = pd.read_csv(data_file_path, encoding=encoding)
 
 print("Cleaning responses...")
-processed_df = df[df.columns[1:]].map(preprocess_text)
-print("\nResponses:\n", processed_df.head(10))
+df_preprocessed = df.iloc[:, 1:].map(preprocess_text)  # type: ignore
+print("\nResponses:\n", df_preprocessed.head(10))
 
 # Load categories
-categories_file_path = ""
+categories_file_path = "categories_output.csv"
 print("Loading categories...")
 with open(categories_file_path, "rb") as file:
     encoding = chardet.detect(file.read())["encoding"]  # Detect encoding
 categories = pd.read_csv(categories_file_path, encoding=encoding)
+print("Categories:\n", categories)
 
 # Create data structures
 categories_list = categories.iloc[:, 0].tolist()
-df_preprocessed = pd.DataFrame(
-    df.iloc[:, 1:].map(
-        preprocess_text  # , na_action="ignore"
-    )  # type: ignore
-)
-unique_responses = set(df_preprocessed.stack().reset_index(drop=True).dropna())
+
+unique_responses = set(df_preprocessed.stack().dropna().reset_index(drop=True)) - {""}
 uuids = df.iloc[:, 0]
 response_columns = list(df_preprocessed.columns)
 categorized_data = pd.concat([uuids, df_preprocessed], axis=1)
@@ -132,22 +151,22 @@ categorize_missing_data(categorized_data)
 # Get GPT responses
 question = "Why were you or your child consuming media at this time?"
 print("Categorizing data with GPT-4...")
-for response in unique_responses:
-    # Call your categorize_response_GPT function
-    gpt_response = categorize_response_GPT(client, question, str(response), categories_list)
+# unique_responses_sample = list(unique_responses)[:20]
+categorized_responses = parallel_gpt_calls(unique_responses, client, question, categories_list)
+print("Finished categorizing with GPT-4...")
 
-    if gpt_response != "Error":
-        categorized_data = categorize_response_in_dataframe(
-            str(response), gpt_response, categorized_data, response_columns
-        )
+print("Preparing data...")
+for response, category in categorized_responses.items():
+    if category != "Error":
+        categorize_response_in_dataframe(response, category, categorized_data, response_columns)
     else:
         print(f"Response '{response}' was not categorized.")
-
-print("Categorized results:\n", categorized_data.head(20))
+categorized_data = categorize_missing_data(categorized_data)
+print("Categorized results:\n", categorized_data.head(10))
 
 # Save to csv
-result_file_path = "categories_output.csv"
+result_file_path = "categorized_data.csv"
 print(f"\nSaving to {result_file_path} ...")
-export_dataframe_to_csv(result_file_path, categorized_data)
+export_dataframe_to_csv(result_file_path, categorized_data, header=True)
 
 print("\nDone")
--- gpt_category_generator.py
+++ gpt_category_generator.py
@@ -25,11 +25,11 @@ def get_random_sample_from_series(series: pd.Series, sample_size: int) -> pd.Ser
     return series.sample(sample_size, random_state=random.randint(1, 10000))
 
 
-def export_dataframe_to_csv(file_path: str, export_df: pd.DataFrame) -> None:
+def export_dataframe_to_csv(file_path: str, export_df: pd.DataFrame, header: bool = False) -> None:
     try:
         if export_df.empty:
             return
-        export_df.to_csv(file_path, index=False, header=False)
+        export_df.to_csv(file_path, index=False, header=header)
     except Exception as e:
         print(f"Error while writing to CSV: {e}")
 
@@ -43,21 +43,24 @@ def generate_categories_GPT(
     system = [
         {
             "role": "system",
-            "content": "You are data analyst, working with open-ended questionnaire responses.",
+            "content": """You are a data analyst specialized in categorizing open-ended responses from questionnaires. 
+            Your task is to assign a list of the most appropriate distinct thematic categories to the sample of responses
+            based on the provided question and response content.""",
         }
     ]
     user = [
         {
             "role": "user",
-            "content": f"""Generate a list of {number_of_categories} distinct thematic categories
-                        for the following responses. Return the category names in the following format,
-                        `["name1", "name2", ...]`\n\n
+            "content": f"""Generate a list of the {number_of_categories} most appropriate distinct thematic categories
+                        for the following responses. Return ONLY the category names, in the format `["name1", "name2", ...]`\n\n
                         Question:\n`{question}`\n\n
                         Responses:\n`{responses_sample}`""",
         }
     ]
     try:
-        completion = client.chat.completions.create(messages=system + user, model="gpt-4")
+        completion = client.chat.completions.create(
+            messages=system + user, model="gpt-4-1106-preview"
+        )
         categories = json.loads(completion.choices[0].message.content)
 
     except Exception as e:
@@ -75,17 +78,19 @@ with open(file_name, "rb") as file:
 df = pd.read_csv(file_name, encoding=encoding)
 
 print("Cleaning responses...")
-processed_responses = df[1].map(preprocess_text).dropna()
-print("\nResponses:\n", processed_responses.head(10))
+processed_responses = df[df.columns[1:]].map(preprocess_text)
+unique_responses = processed_responses.stack().drop_duplicates().dropna().reset_index(drop=True)
+print("\nResponses:\n", unique_responses.head(10))
 
 print("\nFetching sample...")
-responses_sample = get_random_sample_from_series(processed_responses, 100)
+responses_sample = get_random_sample_from_series(unique_responses, 200)
 
 print("Generating categories with GPT-4...")
 questionnaire_question = "Why were you or your child consuming media at this time?"
 categories = generate_categories_GPT(
     client, questionnaire_question, responses_sample, number_of_categories=20
 )
+categories.extend(["Other", "Bad response"])
 categories_df = pd.DataFrame(categories)
 print(f"\nCategories:\n{categories}")
 
