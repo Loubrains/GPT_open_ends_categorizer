ec1767e5b4851f0255bb52071eadded7314d09c3
Loubrains <43915399+Loubrains@users.noreply.github.com>
2024-01-26 20:58:27 +0000

    Wrote docstring for the utils module

---
+++ src/utils/__init__.py
@@ -0,0 +1,18 @@
+"""
+GPT-4 Categorizer package for Open-Ended Survey Responses
+
+This package provides utilities designed for the processing of open-ended survey responses, categorizating them using GPT-4, and formatting the results. 
+
+Modules:
+    general_utils:
+        Contains functions for general data handling and processing tasks, such as text preprocessing, random sampling from series, 
+        batch creation, loading and exporting data to and from CSV files.
+
+    gpt_utils:
+        Provides utilities for interacting with the OpenAI GPT model asyncronously. It includes functions for sending prompts to the GPT model, 
+        generating lists of thematic categories, generating codeframes for responses, validating categorized outputs, and batch processing.
+
+    dataframe_utils:
+        Provides utilities for manipulating DataFrames, with the goal of preparing the categorized data for further analysis.
+        It includes functions for initializing categorized DataFrames, handling missing data, and categorizing responses based on a codeframe.
+"""
