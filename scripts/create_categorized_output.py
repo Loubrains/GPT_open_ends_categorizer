77a78a4a4ea66516351d5f3d8e944ec96bbbba00
Loubrains <43915399+Loubrains@users.noreply.github.com>
2024-01-27 23:50:52 +0000

    Wrapped everything in a try/except block and terminates the script upon error. Importing config as cfg. Using updated split out save/load file paths from config

--- scripts/create_categorized_output.py
+++ scripts/create_categorized_output.py
@@ -22,73 +22,80 @@ Notes:
 - The script utilizes utility functions from `general_utils` and `dataframe_utils` modules.
 - User-defined variables such as file paths should be properly set in the `config.py` file before running this script.
 - The script supports both single-category and multi-category (`is_multicode`) response categorization.
+- The script terminates if exceptions are raised at any point.
 """
 
 import pandas as pd
 import chardet
 from itertools import islice
+import sys
 from gpt_categorizer_utils import general_utils, dataframe_utils
-from config import *
+import config as cfg
 
-# Load open ends
-print("\nLoading data...")
-with open(open_end_data_file_path, "rb") as file:
-    encoding = chardet.detect(file.read())["encoding"]  # Detect encoding
-df = pd.read_csv(open_end_data_file_path, encoding=encoding)
-print(f"\nRaw data:\n{df.head(20)}")
+try:
+    # Load open ends
+    print("\nLoading data...")
+    with open(cfg.open_end_data_file_path_load, "rb") as file:
+        encoding = chardet.detect(file.read())["encoding"]  # Detect encoding
+    df = pd.read_csv(cfg.open_end_data_file_path_load, encoding=encoding)
+    print(f"\nRaw data:\n{df.head(20)}")
 
-# Clean open ends
-print("\nCleaning responses...")
-response_columns = df.iloc[:, 1:].map(general_utils.preprocess_text)
-print(f"\nResponses (first 10):\n{response_columns.head(10)}")
+    # Clean open ends
+    print("\nCleaning responses...")
+    response_columns = df.iloc[:, 1:].map(general_utils.preprocess_text)
+    print(f"\nResponses (first 10):\n{response_columns.head(10)}")
 
-# Load categories
-print("\nLoading categories...")
-with open(categories_file_path, "rb") as file:
-    encoding = chardet.detect(file.read())["encoding"]  # Detect encoding
-categories = pd.read_csv(categories_file_path, encoding=encoding, header=None)
-print(f"\nCategories:\n{categories}")
+    # Load categories
+    print("\nLoading categories...")
+    with open(cfg.categories_file_path_load, "rb") as file:
+        encoding = chardet.detect(file.read())["encoding"]  # Detect encoding
+    categories = pd.read_csv(cfg.categories_file_path_load, encoding=encoding, header=None)
+    print(f"\nCategories:\n{categories}")
 
-# Load codeframe (dictionary of response-category pairs)
-print("\nLoading codeframe...")
-if is_multicode:
-    categorized_dict = general_utils.load_csv_to_dict_of_lists(codeframe_file_path)
-else:
-    categorized_dict = general_utils.load_csv_to_dict(codeframe_file_path)
-print("\nCodeframe (first 10):\n")
-print("\n".join(f"{key}: {value}" for key, value in islice(categorized_dict.items(), 10)))
+    # Load codeframe (dictionary of response-category pairs)
+    print("\nLoading codeframe...")
+    if cfg.is_multicode:
+        categorized_dict = general_utils.load_csv_to_dict_of_lists(cfg.codeframe_file_path_load)
+    else:
+        categorized_dict = general_utils.load_csv_to_dict(cfg.codeframe_file_path_load)
+    print("\nCodeframe (first 10):\n")
+    print("\n".join(f"{key}: {value}" for key, value in islice(categorized_dict.items(), 10)))
 
-# Create data structures
-categories_list = categories.iloc[:, 0].tolist()
-uuids = df.iloc[:, 0]
-response_column_names = list(response_columns.columns)
-categorized_data = pd.concat([uuids, response_columns], axis=1)
-# repeat categories columns for each response column
-categorized_data = dataframe_utils.construct_default_categorized_dataframe(
-    categorized_data, response_column_names, categories_list
-)
-for response_column in response_column_names:
-    categorized_data = dataframe_utils.categorize_missing_data_for_response_column(
-        categorized_data, response_column, categories_list
+    # Create data structures
+    categories_list = categories.iloc[:, 0].tolist()
+    uuids = df.iloc[:, 0]
+    response_column_names = list(response_columns.columns)
+    categorized_data = pd.concat([uuids, response_columns], axis=1)
+    # repeat categories columns for each response column
+    categorized_data = dataframe_utils.construct_default_categorized_dataframe(
+        categorized_data, response_column_names, categories_list
     )
-
-# Populate categorized dataframe
-print("\nPreparing output data...")
-for response_column in response_column_names:
-    for response, categories in categorized_dict.items():
-        if is_multicode and "Error" in categories:
-            print(f"\nResponse '{response}' was not categorized.")
-        elif categories == "Error":
-            print(f"\nResponse '{response}' was not categorized.")
-
-        dataframe_utils.categorize_responses_for_response_column(
-            response, categories, response_column, categorized_data, is_multicode
+    for response_column in response_column_names:
+        categorized_data = dataframe_utils.categorize_missing_data_for_response_column(
+            categorized_data, response_column, categories_list
         )
 
-print(f"\nCategorized results:\n{categorized_data.head(10)}")
+    # Populate categorized dataframe
+    print("\nPreparing output data...")
+    for response_column in response_column_names:
+        for response, categories in categorized_dict.items():
+            if cfg.is_multicode and "Error" in categories:
+                print(f"\nResponse '{response}' was not categorized.")
+            elif categories == "Error":
+                print(f"\nResponse '{response}' was not categorized.")
 
-# Save to csv
-print(f"\nSaving to {categorized_data_file_path} ...")
-general_utils.export_dataframe_to_csv(categorized_data_file_path, categorized_data)
+            dataframe_utils.categorize_responses_for_response_column(
+                response, categories, response_column, categorized_data, cfg.is_multicode
+            )
 
-print("\nFinished")
+    print(f"\nCategorized results:\n{categorized_data.head(10)}")
+
+    # Save to csv
+    print(f"\nSaving to {cfg.categorized_data_file_path_save} ...")
+    general_utils.export_dataframe_to_csv(cfg.categorized_data_file_path_save, categorized_data)
+
+    print("\nFinished")
+
+except Exception as e:
+    print(e)
+    sys.exit(1)
