d2da1c2f2861c35f2e44578d8a1a816e026c759d
Loubrains <43915399+Loubrains@users.noreply.github.com>
2024-01-28 13:36:41 +0000

    Updated obfuscator regex

--- obfuscator.py
+++ obfuscator.py
@@ -5,12 +5,12 @@ import re
 
 def obfuscate_sensitive_data(content):
     replacements = {
-        r'open_end_data_file_path_load = Path(".*?")': 'open_end_data_file_path_load = Path("path/to/your/data")',
-        r'categories_file_path_save = Path(".*?")': 'categories_file_path_save = Path("path/to/save/categories")',
-        r'categories_file_path_load = Path(".*?")': 'categories_file_path_load = Path("path/to/load/categories")',
-        r'codeframe_file_path_save = Path(".*?")': 'codeframe_file_path_save = Path("path/to/save/codeframe")',
-        r'codeframe_file_path_load = Path(".*?")': 'codeframe_file_path_load = Path("path/to/load/codeframe")',
-        r'categorized_data_file_path_save = Path(".*?")': 'categorized_data_file_path_save = Path("path/to/save/categorized/data")',
+        r'open_end_data_file_path_load = Path\(".*?"\)': 'open_end_data_file_path_load = Path("path/to/your/data")',
+        r'categories_file_path_save = Path\(".*?"\)': 'categories_file_path_save = Path("path/to/save/categories")',
+        r'categories_file_path_load = Path\(".*?"\)': 'categories_file_path_load = Path("path/to/load/categories")',
+        r'codeframe_file_path_save = Path\(".*?"\)': 'codeframe_file_path_save = Path("path/to/save/codeframe")',
+        r'codeframe_file_path_load = Path\(".*?"\)': 'codeframe_file_path_load = Path("path/to/load/codeframe")',
+        r'categorized_data_file_path_save = Path\(".*?"\)': 'categorized_data_file_path_save = Path("path/to/save/categorized/data")',
         r'questionnaire_question = ".*?"': 'questionnaire_question = "your_questionnaire_question"',
     }
 
