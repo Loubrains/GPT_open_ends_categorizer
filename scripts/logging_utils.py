bf18baa8fdbfb95233b0e62a1aa9c1db4b0dd83e
Loubrains <louie.atk@gmail.com>
2024-01-30 11:12:03 +0000

    Updated logging utils

--- scripts/logging_utils.py
+++ scripts/logging_utils.py
@@ -6,17 +6,18 @@ def setup_logging():
     logging.basicConfig(
         filename="app.log",
         filemode="w",
-        level=logging.INFO,
+        level=logging.WARNING,  # suppress all loggers from dependencies
         format="%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s",
     )
 
-    logger = logging.getLogger()
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
 
     # Set up console logging
     console_handler = logging.StreamHandler()
-    console_format = logging.Formatter("%(levelname)s - %(message)s")
+    console_format = logging.Formatter("%(message)s")
     console_handler.setFormatter(console_format)
-    console_handler.setLevel(logging.ERROR)
+    console_handler.setLevel(logging.INFO)
     logger.addHandler(console_handler)
 
     # Suppress noisey libraries
